d0288c1cd42971959884a00b32c4afdf7df20bd5
曹亚丽 <YALI@LAPTOP-0DGE79N6>
2018-07-04 10:30:59 +0800

    更新了answers的接口代码，对其进行进一步的完善。

--- src/main/java/com/briup/apps/poll/service/impl/AnswersServiceImpl.java
+++ src/main/java/com/briup/apps/poll/service/impl/AnswersServiceImpl.java
@@ -8,7 +8,9 @@ import com.briup.apps.poll.bean.Answers;
 import com.briup.apps.poll.bean.AnswersExample;
 import com.briup.apps.poll.bean.Course;
 import com.briup.apps.poll.bean.CourseExample;
+import com.briup.apps.poll.bean.extend.AnswersVM;
 import com.briup.apps.poll.dao.AnswersMapper;
+import com.briup.apps.poll.dao.extend.AnswersVMMapper;
 import com.briup.apps.poll.service.IAnswersService;
 
 
@@ -17,7 +19,8 @@ import com.briup.apps.poll.service.IAnswersService;
 public class AnswersServiceImpl implements IAnswersService {
 	@Autowired
 	private AnswersMapper answersMapper;
-
+    @Autowired
+    private AnswersVMMapper  answersVMMapper;
 	@Override
 	public void saveOrUpdate(Answers answers) throws Exception {
 		// TODO Auto-generated method stub
@@ -42,6 +45,11 @@ public class AnswersServiceImpl implements IAnswersService {
 	public void deleteById(long id) throws Exception {
 		// TODO Auto-generated method stub
 		answersMapper.deleteByPrimaryKey(id);
+	}
+
+	@Override
+	public List<AnswersVM> findAllAnswersVM() throws Exception {
+		return answersVMMapper.selectAll();
 	}
 
 
